d8c51d3f0c454ab47b33bfcee31fd7da456b4cda
Dakshin G <dakshing006@gmail.com>
2025-06-13 00:23:44 +0530

    Update PropertiesConfig.java

--- src/main/java/com/minikube/sample/properties/PropertiesConfig.java
+++ src/main/java/com/minikube/sample/properties/PropertiesConfig.java
@@ -1,16 +1,11 @@
 package com.minikube.sample.properties;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
 @ConfigurationProperties(prefix = "app.data")
-@Getter
-@Setter
 public class PropertiesConfig {
 
 	private String test;
